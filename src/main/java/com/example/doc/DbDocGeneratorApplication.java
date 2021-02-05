25013ae208950dbbb43db0a6f4aec958a4183eff
wuhengzhen <wuhengzhen@lianxin-tech.com>
2021-02-05 17:19:14 +0800

    first commit

---
+++ src/main/java/com/example/doc/DbDocGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbDocGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbDocGeneratorApplication.class, args);
+    }
+
+}
